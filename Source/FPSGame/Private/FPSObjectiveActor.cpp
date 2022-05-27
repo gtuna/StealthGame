03fb6bf5529d23f83ec27856b7af00e3cff169bc
tuna <gtuna52@gmail.com>
2022-05-27 10:27:15 +0300

    setting up collision

--- Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "FPSObjectiveActor.h"
+#include "Kismet/GameplayStatics.h"
 
 
 // Sets default values
@@ -11,10 +13,15 @@ AFPSObjectiveActor::AFPSObjectiveActor()
 	PrimaryActorTick.bCanEverTick = true;
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	RootComponent = MeshComp;
 
 	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly); // only detect overlap, but don't react
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore); // ignore all collision responses
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap); // only react to overlap with pawns !
 	SphereComp->SetupAttachment(MeshComp);
+
 }
 
 // Called when the game starts or when spawned
@@ -24,10 +31,22 @@ void AFPSObjectiveActor::BeginPlay()
 	
 }
 
+void AFPSObjectiveActor::PlayEffects()
+{
+	UGameplayStatics::SpawnEmitterAtLocation( this, PickupFX, GetActorLocation());
+}
+
 // Called every frame
 void AFPSObjectiveActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	PlayEffects();
 }
 
