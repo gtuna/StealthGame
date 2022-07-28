ab5e6d72d7a2ae867cc91b66aaab01b595da4c2c
tuna <gtuna52@gmail.com>
2022-07-28 13:31:41 +0300

    adding AI guard class

---
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Perception/PawnSensingComponent.h"
+
+#include "FPSAIGuard.h"
+
+// Sets default values
+AFPSAIGuard::AFPSAIGuard()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComponent"));
+	
+	
+}
+
+// Called when the game starts or when spawned
+void AFPSAIGuard::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPSAIGuard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
